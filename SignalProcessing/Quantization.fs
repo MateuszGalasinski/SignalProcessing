e234dde119e49b9fc8cef81bd16b6df5e98c2de9
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2019-03-31 12:12:45 +0200

    started quantization work

---
+++ SignalProcessing/Quantization.fs
@@ -0,0 +1,10 @@
+﻿namespace SignalProcessing
+
+module Quantization = 
+    open System
+
+    let quantizate (points:List<Point>) bits = 
+        let lvlAmount = int (2.0 ** bits)
+        let miny,maxy = 
+            points |> List.fold (fun (my,My) (p) -> 
+                min my p.y.r, max My p.y.r) (Double.MaxValue,Double.MinValue)
