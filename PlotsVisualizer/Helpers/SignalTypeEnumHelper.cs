441bb56677b108e6caa29844a3e08e7a5ee57922
Mateusz Galasinski <Mateusz.Galasinski@vistex.com>
2019-05-12 20:05:36 +0200

    reordered window

--- PlotsVisualizer/Helpers/SignalTypeEnumHelper.cs
+++ PlotsVisualizer/Helpers/SignalTypeEnumHelper.cs
@@ -1,7 +1,7 @@
-﻿using SignalProcessing;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SignalProcessing;
 
 namespace PlotsVisualizer.Helpers
 {
@@ -10,7 +10,7 @@ namespace PlotsVisualizer.Helpers
         public static IEnumerable<string> GetAllValues(Type t)
         {
             if (t != typeof(Types.SignalType))
-                throw new ArgumentException($"{nameof(t)} must be an enum type");
+                throw new ArgumentException($"{nameof(t)} must be an enum signal type");
 
             return Enum.GetValues(t).Cast<Types.SignalType>().Where(s => s != Types.SignalType.Composed).Select(e => e.ToString()).ToList();
         }
--- SignalProcessing/Filters.fs
+++ SignalProcessing/Filters.fs
@@ -1,25 +1,31 @@
 ﻿namespace SignalProcessing
 
 module Filters =
+    open System
     open MathNet.Numerics
 
     type WindowFunction =
-       | FlatTop
-       | Hamming
-       | Hanning
-       | Blackman
+       | FlatTop = 0
+       | Hamming = 1
+       | Hanning = 2
+       | Blackman = 3
 
     let resolveWindow windowType = 
         match windowType with 
-        | FlatTop -> Window.FlatTop
-        | Hamming -> Window.Hamming
-        | Hanning -> Window.Hann
-        | Blackman -> Window.Blackman
+        | WindowFunction.FlatTop -> Window.FlatTop
+        | WindowFunction.Hamming -> Window.Hamming
+        | WindowFunction.Hanning -> Window.Hann
+        | WindowFunction.Blackman -> Window.Blackman
 
-    let generateFilter windowType M K samplingFrequency = 
+    type FilterType =
+       | LowPass = 0
+       | BandPass = 1
+       | HighPass = 2
+
+    let generateLowPassFilterFilter windowType M K samplingFrequency = 
         let coefficients = resolveWindow windowType M
         List.ofArray coefficients
-        |> List.map2 (fun y x -> Point(x, Complex(double(y), 0.0))) [0..coefficients.Length-1]
+        |> List.map2 (fun x y -> Point(x, Complex(y, 0.0))) [0.0..1.0..double(coefficients.Length-1)]
         |> (fun points -> 
         {
             metadata = 
@@ -35,4 +41,23 @@ module Filters =
                 }
             points = points
         })
-    
+
+    let filterShifter filter shiftFunction = 
+        {
+            filter with points = filter.points
+                    |> List.map2 (fun i p -> Point(p.x, Complex(shiftFunction(i) * p.y.r, p.y.i))) [0.0..1.0..double(filter.points.Length-1)]
+        }
+        
+
+    let makeHighPassFilter (filter:Signal) = 
+        filterShifter filter (fun n -> (-1.0)**n)
+
+    let makeBandPassFilter (filter:Signal) = 
+        filterShifter filter (fun n -> 2.0 * sin(Math.PI * n / 2.0))
+
+    let createFilter filterType windowType M K samplingFrequency =
+        let lowPassFilter = generateLowPassFilterFilter windowType M K samplingFrequency
+        match filterType with 
+        | FilterType.LowPass -> lowPassFilter
+        | FilterType.BandPass -> makeBandPassFilter lowPassFilter
+        | FilterType.HighPass -> makeHighPassFilter lowPassFilter
